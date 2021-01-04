e886fc459dd99159225ba2ccf7efc265cb201ca5
Kristianped <55670926+Kristianped@users.noreply.github.com>
2021-01-04 13:22:10 +0100

    #11 closed

--- pet-clinic-data/src/main/java/no/kristianped/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/no/kristianped/petclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package no.kristianped.petclinic.services;
 
 import no.kristianped.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findByd(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/no/kristianped/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/no/kristianped/petclinic/services/VetService.java
@@ -2,13 +2,5 @@ package no.kristianped.petclinic.services;
 
 import no.kristianped.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findByd(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
