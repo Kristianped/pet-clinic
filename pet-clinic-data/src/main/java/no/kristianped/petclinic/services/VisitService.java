0caf527b1362c1b2a90de7a9a6947124bee5e0ec
Kristianped <55670926+Kristianped@users.noreply.github.com>
2021-01-06 15:15:15 +0100

    Add Map Service for Visit. Closes #46

---
+++ pet-clinic-data/src/main/java/no/kristianped/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package no.kristianped.petclinic.services;
+
+import no.kristianped.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/no/kristianped/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package no.kristianped.petclinic.services.map;
+
+import no.kristianped.petclinic.model.Visit;
+import no.kristianped.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object == null)
+            return null;
+
+        if (object.getPet() == null || object.getPet().getOwner() == null
+                || object.getPet().getId() == null || object.getPet().getOwner().getId() == null)
+            throw new RuntimeException("Invalid visit");
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
