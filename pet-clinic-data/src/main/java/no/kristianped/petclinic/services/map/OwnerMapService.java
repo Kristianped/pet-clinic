c16482694cc55325b1ac71f54cd925d7f4ba95f2
Kristianped <55670926+Kristianped@users.noreply.github.com>
2021-01-06 09:33:05 +0100

    Enhance owners with pet and contact into on startup. Closes #33

--- pet-clinic-data/src/main/java/no/kristianped/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/no/kristianped/petclinic/services/map/OwnerMapService.java
@@ -1,13 +1,24 @@
 package no.kristianped.petclinic.services.map;
 
 import no.kristianped.petclinic.model.Owner;
+import no.kristianped.petclinic.model.Pet;
 import no.kristianped.petclinic.services.OwnerService;
+import no.kristianped.petclinic.services.PetService;
+import no.kristianped.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -26,6 +37,26 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
+        if (object == null)
+            return null;
+
+        if (object.getPets() != null) {
+            object.getPets().forEach(pet -> {
+                if (pet.getPetType() != null) {
+                    if (pet.getPetType().getId() == null) {
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    }
+                } else {
+                    throw new RuntimeException("Pet Type is required");
+                }
+
+                if (pet.getId() == null) {
+                    Pet savedPet = petService.save(pet);
+                    pet.setId(savedPet.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/no/kristianped/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/no/kristianped/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package no.kristianped.petclinic.bootstrap;
 
 import no.kristianped.petclinic.model.Owner;
+import no.kristianped.petclinic.model.Pet;
 import no.kristianped.petclinic.model.PetType;
 import no.kristianped.petclinic.model.Vet;
 import no.kristianped.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import no.kristianped.petclinic.services.PetTypeService;
 import no.kristianped.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -35,11 +38,31 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("12311234443");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogType);
+        mikesPet.setName("Rosco");
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setOwner(owner1);
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
-        owner2.setLastName("Glename");
+        owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("12311234443");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setPetType(cat);
+        fionasCat.setName("Just cat");
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setOwner(owner2);
+        owner2.getPets().add(fionasCat);
         ownerService.save(owner2);
 
         System.out.println("Loaded some owners");
